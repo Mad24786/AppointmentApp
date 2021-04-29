50f53ec2c823376df212d46cb1d08d89e5bec091
Mathias Fernahl <mfern@DESKTOP-6TKK4I9.fritz.box>
2021-04-29 10:31:46 +0200

    Dashboard Calendar added

--- AppointmentApp/src/de/nrw/hspv/views/DashboardPanel.java
+++ AppointmentApp/src/de/nrw/hspv/views/DashboardPanel.java
@@ -3,8 +3,13 @@ package de.nrw.hspv.views;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,16 +25,64 @@ public class DashboardPanel extends JPanel {
 	}
 	
 	public void initComponents(){
+		
+		/** Initialize calendar vars */
+		int selectedMonth, selectedYear;
+		Calendar cal = new GregorianCalendar();
+		int cDay = cal.get(Calendar.DATE);
+		int cMonth = selectedMonth = cal.get(Calendar.MONTH);
+		int cYear = selectedYear = cal.get(Calendar.YEAR);
+		
+		/** */
 		setLayout(new BorderLayout());
 		
+		JPanel northPanel = new JPanel();
+		northPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		northPanel.setBackground(Color.WHITE);
+		JLabel lblDate = new JLabel(monthToString(cMonth) + " " + cYear);
+		northPanel.add(lblDate);
+		add(northPanel, BorderLayout.NORTH);
 		
 		JPanel centerPanel = new JPanel();		
 		centerPanel.setLayout(new GridLayout(5, 7, 5, 5));
 		centerPanel.setBackground(Color.WHITE);
-		for (int i = 0; i < 35; i++) {
-			JPanel panel = new CalendarPanel(i);
-			centerPanel.add(panel);
-		}	
+		
+		/** TEST BEGINN **/
+		GregorianCalendar gCal = new GregorianCalendar(selectedYear, selectedMonth, 1);
+		int days =gCal.getActualMaximum(Calendar.DATE);
+		int startInWeek = gCal.get(Calendar.DAY_OF_WEEK);
+		
+		gCal = new GregorianCalendar(selectedYear, selectedMonth, 1);
+		int totalweeks = gCal.getActualMaximum(Calendar.WEEK_OF_MONTH);
+		
+		int count = 1; // count the days
+		for (int i = 1; i <= totalweeks; i++) {
+			for(int j = 1; j <= 7; j++) {
+				JPanel panel;
+				if (count < startInWeek || (count - startInWeek + 1) > days) {
+					// no day here
+					panel = new CalendarPanel(0, Color.WHITE);
+					centerPanel.add(panel);
+				}
+				else {
+					if(cDay == (count - startInWeek + 1) && cMonth == selectedMonth && cYear == selectedYear) {
+						// marks the current day
+						panel = new CalendarPanel((count - startInWeek + 1), HspvColor.ORANGE);
+						centerPanel.add(panel);
+					}
+					else {
+						// every other day
+						panel = new CalendarPanel((count - startInWeek + 1), HspvColor.SEC_BROWN);
+						centerPanel.add(panel);
+					}
+					panel.addMouseListener(new MyMouseListener());
+				}
+				count++;
+			}
+			
+		}
+		
+		/** TEST ENDE **/
 
 		
 		JPanel eastPanel = new JPanel();
@@ -46,25 +99,57 @@ public class DashboardPanel extends JPanel {
 		
 	}
 	
+	public String monthToString(int m) {
+		switch (m) {
+		case 0: {
+			return "Januar";
+		}
+		case 1: {
+			return "Feburar";
+		}
+		case 2: {
+			return "März";
+		}
+		case 3: {
+			return "April";
+		}
+		default:
+			return "ein anderer Monat";
+		}
+	}
+	
+	public class MyMouseListener extends MouseAdapter{
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			System.out.println(((CalendarPanel) e.getComponent()).getDay());
+		}
+	}
+	
 	public class CalendarPanel extends JPanel {
 
-		int i;
+		int day;
 		
-		public CalendarPanel(int i) {
+		public CalendarPanel(int day, Color c) {
 			
-			this.i = i;
+			this.day = day;
 			
 			setPreferredSize(new Dimension(75,75));
 			setMaximumSize(new Dimension(75,75));
 			
-			setBackground(HspvColor.SEC_BROWN);
+			setBackground(c);
 			
 		}
 		
 		@Override
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			g.drawString(String.valueOf(i), 5, 15);
+			if(day != 0) {
+				g.drawString(String.valueOf(day), 5, 15);
+			}
+		}
+
+		public int getDay() {
+			return day;
 		}
 
 	}
